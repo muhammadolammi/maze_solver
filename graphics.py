38e36f6248eb6c2e5a339e6cf267b6e138f27b0e
muhammad_olammi <muhammadolammi@gmail.com>
2023-11-17 17:04:40 +0100

    recreate maze class

--- graphics.py
+++ graphics.py
@@ -1,14 +1,25 @@
 from tkinter import Tk, BOTH, Canvas
-
-
+class Point:
+    def __init__(self, x, y) :
+        self.x =x
+        self.y =y
+class Line:
+    def __init__(self, point1:Point, point2:Point):
+        self.x1=point1.x
+        self.x2=point2.x
+        self.y1 =point1.y
+        self.y2 = point2.y
+    def draw_line(self, canvas:Canvas, fill_color):
+        canvas.create_line(self.x1, self.y1, self.x2,self.y2,fill=fill_color)
+        canvas.pack()
 class Window:
     def __init__(self, width, height):
         self.__root = Tk()
         self.__root.title("Maze Solver")
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
         self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
         self.__canvas.pack(fill=BOTH, expand=1)
         self.__running = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
     def redraw(self):
         self.__root.update_idletasks()
@@ -20,30 +31,53 @@ class Window:
             self.redraw()
         print("window closed...")
 
-    def draw_line(self, line, fill_color="black"):
-        line.draw(self.__canvas, fill_color)
-
     def close(self):
         self.__running = False
+    
+    def draw_line(self, line:Line, fill_color):
+        line.draw_line(self.__canvas, fill_color)
 
+    def draw_move(self, to_cell, undo=False):
+        if self._win is None:
+            return
+        x_mid = (self._x1 + self._x2) / 2
+        y_mid = (self._y1 + self._y2) / 2
 
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+        to_x_mid = (to_cell._x1 + to_cell._x2) / 2
+        to_y_mid = (to_cell._y1 + to_cell._y2) / 2
 
+        fill_color = "red"
+        if undo:
+            fill_color = "gray"
 
-class Line:
-    def __init__(
-        self,
-        p1,
-        p2,
-    ):
-        self.p1 = p1
-        self.p2 = p2
+            # moving left
+        if self._x1 > to_cell._x1:
+            line = Line(Point(self._x1, y_mid), Point(x_mid, y_mid))
+            self._win.draw_line(line, fill_color)
+            line = Line(Point(to_x_mid, to_y_mid), Point(to_cell._x2, to_y_mid))
+            self._win.draw_line(line, fill_color)
+            print("left")
 
-    def draw(self, canvas, fill_color="black"):
-        canvas.create_line(
-            self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2
-        )
-        canvas.pack(fill=BOTH, expand=1)
+            # moving right
+        elif self._x1 < to_cell._x1:
+            line = Line(Point(x_mid, y_mid), Point(self._x2, y_mid))
+            self._win.draw_line(line, fill_color)
+            line = Line(Point(to_cell._x1, to_y_mid), Point(to_x_mid, to_y_mid))
+            self._win.draw_line(line, fill_color)
+            print("right")
+
+            # moving up
+        elif self._y1 > to_cell._y1:
+            line = Line(Point(x_mid, y_mid), Point(x_mid, self._y1))
+            self._win.draw_line(line, fill_color)
+            line = Line(Point(to_x_mid, to_cell._y2), Point(to_x_mid, to_y_mid))
+            self._win.draw_line(line, fill_color)
+            print("up")
+
+            # moving down
+        elif self._y1 < to_cell._y1:
+            line = Line(Point(x_mid, y_mid), Point(x_mid, self._y2))
+            self._win.draw_line(line, fill_color)
+            line = Line(Point(to_x_mid, to_y_mid), Point(to_x_mid, to_cell._y1))
+            self._win.draw_line(line, fill_color)
+            print("down")
